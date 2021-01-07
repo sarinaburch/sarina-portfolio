275793665c1490899ad72297ded61f0556ce6e0d
Sarina Burch <sarinaburch52@gmail.com>
2021-01-07 16:19:20 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Portfolio/precache-manifest.145e04ec128292f72f0fcefb4b0fc3cd.js"
+  "/React-Portfolio/precache-manifest.bde559008b7a42eb44ec9966ae6a0899.js"
 );
 
 self.addEventListener('message', (event) => {
